ab160935927a4dae3a9bc8786a6f61adf41f5349
Jakub Biesek <jakub.webdesign@gmail.com>
2018-06-19 18:27:30 +0100

    add more rooms to chat app

--- index.js
+++ index.js
@@ -7,22 +7,31 @@ server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-app.get('/',(req,res) =>{
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
+app.get('/php', (req, res) => {
+    res.sendFile(__dirname + '/public/php.html');
+});
+
+app.get('/java', (req, res) => {
+    res.sendFile(__dirname + '/public/php.html');
+});
+
+
 var nodeSpace = io.of('/nodeJs');
 
-nodeSpace.on('connection', (socket) =>{
-    socket.on('join',(data)=>{
+nodeSpace.on('connection', (socket) => {
+    socket.on('join', (data) => {
         socket.join(data);
-        nodeSpace.in(data).emit('room',`New user just joined to the room : ${data}`);
+        nodeSpace.in(data).emit('room', `New user just joined to the room : ${data}`);
     });
-    socket.on('msg1',(data)=>{
+    socket.on('msg1', (data) => {
         console.log(`message is ${data.msg}`);
-        nodeSpace.in(data.space.room).emit('messageInput',data.msg);
+        nodeSpace.in(data.space.room).emit('messageInput', data.msg);
     });
     socket.on('disconnect', () => {
-        nodeSpace.emit('messageInput','user disconnected');
+        nodeSpace.emit('messageInput', 'user disconnected');
     });
 });
