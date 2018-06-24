78c0c2fd6c6bcbc708ef41cb85a70b68a516b873
Jakub Biesek <36658591+JK-Biesek@users.noreply.github.com>
2018-06-24 17:06:52 +0100

    Test mocha chai (#3)
    * add test cases to cover my chat app
    
    * add more test cases

--- test/indexTest.js
+++ test/indexTest.js
@@ -6,6 +6,12 @@ describe("Chat App", () => {
     it("Loads home page", (done) => {
         request(app).get("/").expect(200).end(done);
     });
+    it("Loads php page", (done) => {
+        request(app).get("/php").expect(200).end(done);
+    });
+    it("Loads java page", (done) => {
+        request(app).get("/java").expect(200).end(done);
+    });
     it("should fail getting not existing route", (done) => {
         request(app).get("/notFound").expect(404).end(done);
     })
